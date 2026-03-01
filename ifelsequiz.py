7daa32ae6de8e6b65aba7e3cc16c94ddaca267e8
Leena Hayat <leenahayat14@gmail.com>
2026-03-01 21:07:24 +0500

    Add files via upload

---
+++ ifelsequiz.py
@@ -0,0 +1,15 @@
+import time
+
+timestamp = time.strftime("%H:%M:%S")
+
+
+print("Current time:", timestamp)
+
+hour = int(time.strftime('%H'))
+
+if hour < 12:
+    print("Good Morning")
+elif hour < 18:
+    print("Good Evening")
+else:
+    print("Good Night")
