1bf382c3888dd364beb4b3a3eebff34a090ecbd6
Leena Hayat <leenahayat14@gmail.com>
2026-03-01 21:49:38 +0500

    Update ifelsequiz.py

--- ifelsequiz.py
+++ ifelsequiz.py
@@ -7,9 +7,9 @@ print("Current time:", timestamp)
 
 hour = int(time.strftime('%H'))
 
-if hour < 12:
+if (hour < 12):
     print("Good Morning")
-elif hour < 18:
+elif (hour < 18):
     print("Good Evening")
 else:
     print("Good Night")
