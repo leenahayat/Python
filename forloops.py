d1796a3fdfce3231ad2c4545f6033c7961270014
Leena Hayat <leenahayat14@gmail.com>
2026-03-02 22:00:13 +0500

    Add files via upload

---
+++ forloops.py
@@ -0,0 +1,26 @@
+# Loop in list
+
+colors = ["Red" , "Yellow" , "Orange" , "Pink"]
+for i in colors:
+    print(i)
+    for z in i:
+     print(z)
+
+print("\n")
+#  range(
+#     start: SupportsIndex,
+#     stop: SupportsIndex,
+#     step: SupportsIndex = 1,
+#     /
+# )
+# using range() function
+
+for i in range(5):
+   print(i)     
+
+print("\n")
+
+# using range() function
+
+for i in range(1, 20 , 3):
+   print(i)      
