23fcd06df59c3f7ec8c22337462753460ed65600
Leena Hayat <leenahayat14@gmail.com>
2026-03-02 21:27:40 +0500

    Add files via upload

---
+++ matchcasestat.py
@@ -0,0 +1,9 @@
+x = int(input("Enter any number: "))
+
+match x:
+    case 0:
+        print("Number is zero.")
+    case 4:
+        print("Number is zero.")
+    case _:
+        print("Number entered by user is",x)
