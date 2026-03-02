d3fa278920f96f0ed20fc5ee3f3eeae375ce7aa9
Leena Hayat <leenahayat14@gmail.com>
2026-03-02 22:20:11 +0500

    Add files via upload

---
+++ whileloop.py
@@ -0,0 +1,24 @@
+print("Incrementing while loop.")
+#incrementing while loop
+i = 0
+while(i<3):
+    print(i)
+    i = i +1
+
+    
+print("\nDecrementing while loop.")
+#decrementing while loop
+count = 5
+while(count>0):
+    print(count)
+    count = count - 1  
+
+
+print("\nUsing else with while loop.")
+#else with while loop
+x = 5
+while(x>0):
+    print(x)
+    x = x - 1 
+else:
+    print("counter is 0")    
