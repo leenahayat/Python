49ccd32e49d0cc2a9e7152fb952b906eef39d604
Leena Hayat <leenahayat14@gmail.com>
2026-03-04 20:17:17 +0500

    Add files via upload

---
+++ break.py
@@ -0,0 +1,17 @@
+for i in range(12):
+  if(i == 10):
+    break
+  print("5 X",i+1,"=",5*(i+1))
+
+
+#break means loop ko chorh k nikal jao
+#continue means iteration ko chorh k nikal jao  
+
+
+for i in range(1, 101,1):
+  print(i, end = " ")
+  if(i==50):
+    break
+  else:
+    print("Mississipi")
+print("Thank you")    
