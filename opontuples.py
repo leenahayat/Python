ee5e8e7a53bf500b749fe74fd4048657adff8ac8
Leena Hayat <leenahayat14@gmail.com>
2026-03-07 22:13:32 +0500

    Operation on tuples

--- opontuples.py
+++ opontuples.py
@@ -7,3 +7,12 @@ temp.pop(3)                           #add item
 temp[2] = "Finland"                   #remove item
 countries = tuple(temp)               #change item
 print(countries)
+
+tuple1 = (0, 1, 2, 3, 2, 31, 1, 3, 2, 3)
+res = tuple1.count(3)
+res = tuple1.index(3)
+# res = tuple1.index(311)
+res = tuple1.index(3, 4, 8)
+print(tuple1)
+res = len(tuple1)
+print('Count of 3 in tuple1 is:', res)
