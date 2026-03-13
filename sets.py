1f36a4bfa198a264a9a179d87841bfb8cb2319bb
Leena Hayat <leenahayat14@gmail.com>
2026-03-13 22:08:20 +0500

    Sets in python

---
+++ sets.py
@@ -0,0 +1,13 @@
+'''sets are unordered collection of data items.They store multiple values in a single variable
+sets do not contain duplicate items
+sets are unchangeable
+stes are enclosed with curly brackets{}'''
+
+s = {2,4,2,6,5,3}
+print(s)
+
+r = set()
+print(type(r))
+
+for value in s:
+    print(value)
